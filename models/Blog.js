5c93c120472dcfda45a55687bb23d58882b79aad
CameronGillum <GillumTCameron@gmail.com>
2024-07-24 11:25:24 -0400

    Changing basic file names

--- models/Blog.js
+++ models/Blog.js
@@ -2,13 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
+class Blog extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-User.init(
+Blog.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -20,34 +20,30 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
+    topic: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
+    created: {
+      type: DataTypes.DATE,
     },
   },
   {
     hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
+      beforeCreate: async (newBlogData) => {
+        newBlogData.password = await bcrypt.hash(newBlogData.password, 10);
+        return newBlogData;
+      },
+      beforeUpdate: async (updatedBlogData) => {
+        updatedBlogData.password = await bcrypt.hash(updatedBlogData.password, 10);
+        return updatedBlogData;
       },
     },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: 'blog',
   }
 );
 
